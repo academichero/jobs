eb1368a83aeb192349f9061ea1e58a46fd9f9b6a
André de Alencar <andrealencar@outlook.com.br>
2016-06-23 08:25:47 -0300

    Adição de campos de acordo com requisitos levantados (#12)

--- modules/board/models.py
+++ modules/board/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Job(models.Model):
@@ -10,6 +11,10 @@ class Job(models.Model):
     description = models.TextField()
     salary_min = models.DecimalField(max_digits=10, decimal_places=2)
     salary_max = models.DecimalField(max_digits=10, decimal_places=2)
+    workload = models.PositiveIntegerField(validators =[MinValueValidator(0), MaxValueValidator(45)])
+    validity = models.DateTimeField()
+    open_job = models.BooleanField(default=True)
+    internal_job = models.BooleanField(default=False)
     author = models.ForeignKey(User)
     featured = models.BooleanField(default=False)
 
